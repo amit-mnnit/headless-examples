c72503149ca07c8c197d0593d9da5eddfa2a3b1b
Amit Kumar <amitkumar@adobe.com>
2024-10-01 10:46:40 +0530

    fix proxy

--- basic-react-apps/src/utils/utils.ts
+++ basic-react-apps/src/utils/utils.ts
@@ -1,10 +1,10 @@
 
-const {REACT_APP_FETCH_FROM_AEM, REACT_APP_SERVER_PORT, REACT_APP_AEM_FORM_PATH } = process.env;
+const {REACT_APP_FETCH_FROM_AEM, REACT_APP_AEM_FORM_PATH } = process.env;
 
 export const getURL = () =>{
   if(REACT_APP_FETCH_FROM_AEM === 'true'){
     return `/content/forms/af/${REACT_APP_AEM_FORM_PATH}/jcr:content/guideContainer.model.json`;
   }else {
-    return `http://localhost:${REACT_APP_SERVER_PORT}/content/core-components-examples/library/adaptive-form/textinput/_jcr_content/root/responsivegrid/demo/component/guideContainer/container/textinput_demo.model.json`
+    return `https://www.aemcomponents.dev/content/core-components-examples/library/adaptive-form/verticaltabs/jcr:content/root/responsivegrid/demo/component/guideContainer.model.json`
   }
 }
